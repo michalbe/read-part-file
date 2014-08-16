e5f188385465befd6cf27fbb8b2cf9d0e7e40170
Michal Budzynski <michal@virtualdesign.pl>
2014-08-16 18:24:34 +0200

    [PART-FILE] - :cry: rename package in tests

--- tests/read-part-file-tests.js
+++ tests/read-part-file-tests.js
@@ -1,27 +1,27 @@
 'use strict';
 
 var assert = require('assert');
-var pf = require('../');
+var rpf = require('../');
 
 var pathToTestFile = './LICENSE';
 
 // Grab first 10 bytes of the source file
-pf(pathToTestFile, 0, 10, function(err, data) {
+rpf(pathToTestFile, 0, 10, function(err, data) {
   assert.equal(err, undefined);
   assert.equal(data.toString(), 'The MIT Li');
 });
 
 // Start greater than zero
-pf(pathToTestFile, 50, 10, function(err, data) {
+rpf(pathToTestFile, 50, 10, function(err, data) {
   assert.equal(data.toString(), 'Budzyński');
 });
 
 // Grab 1MB (should be trancated to 1095 characters)
-pf(pathToTestFile, 0, 1024 * 1024, function(err, data) {
+rpf(pathToTestFile, 0, 1024 * 1024, function(err, data) {
   assert.equal(data.toString().length, 1095);
 });
 
 // File doesn't exists
-pf('hwdp.jczc', 1024*1024, 100, function(err, data) {
+rpf('hwdp.jczc', 1024*1024, 100, function(err, data) {
   assert(err.message.indexOf('ENOENT') > -1);
 });
