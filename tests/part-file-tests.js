07f04bfcb1c5fd9ea274775227dc264cb09c6c8a
Michal Budzynski <michal@virtualdesign.pl>
2014-08-16 18:08:56 +0200

    [PART-FILE] - :angry: tests added

--- tests/part-file-tests.js
+++ tests/part-file-tests.js
@@ -1,7 +1,17 @@
 'use strict';
 
 var assert = require('assert');
-//var part-file = require('../');
+var pf = require('../');
 
-// test 1
-assert(true);
+var pathToTestFile = './LICENSE';
+
+// Grab first 10 bytes of the source file
+pf(pathToTestFile, 0, 10, function(err, data) {
+  assert.equal(err, undefined);
+  assert.equal(data.toString(), 'The MIT Li');
+});
+
+// Grab 1MB (should be trancated to 1095 characters)
+pf(pathToTestFile, 0, 1024 * 1024, function(err, data) {
+  assert.equal(data.toString().length, 1095);
+});
